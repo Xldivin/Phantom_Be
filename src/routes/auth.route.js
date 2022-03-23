c04d0cc84aa8c47de0bae1a2e347a44cbd920abe
shyaka axel divin <Xldivin@gmail.com>
2022-03-23 11:03:21 +0200

    fx(auth-1)

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login_post, signup_post, userProfile,getAllUsers,deleteUser } from '../controllers/auth.controller';
+import { checkAuth, checkAdminAuth } from "../middleware/check-auth";
 
 
 const router = express.Router();
@@ -7,8 +8,8 @@ const router = express.Router();
 router.post('/signup', signup_post);
 router.post('/login', login_post);
 router.get('/user', userProfile);
-router.get('/allusers', getAllUsers);
-router.delete('/user/:id', deleteUser);
+router.get('/allusers',checkAdminAuth, getAllUsers);
+router.delete('/user/:id',checkAdminAuth, deleteUser);
 
 
 
